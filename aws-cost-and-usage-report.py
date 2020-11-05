3a1d130b6a44291d981cef0d817d50f6366d930e
kbroughton-praetorian <kesten.broughton@praetorian.com>
2020-11-04 19:12:38 -1000

    add profile, granularity args

--- aws-cost-and-usage-report.py
+++ aws-cost-and-usage-report.py
@@ -3,37 +3,54 @@
 import argparse
 import boto3
 import datetime
+import json
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--days', type=int, default=30)
+parser.add_argument('--profiles', type=str, default='default')
+parser.add_argument('--granularity', type=str, default='MONTHLY')
 args = parser.parse_args()
 
+profiles = args.profiles.split(',')
+granularity = args.granularity
 
 now = datetime.datetime.utcnow()
 start = (now - datetime.timedelta(days=args.days)).strftime('%Y-%m-%d')
 end = now.strftime('%Y-%m-%d')
 
+# loop over profiles key=profile, value=results
+profiles_results = {}
+# remove any data below MIN_COST
+filtered_results = {}
+MIN_COST = 0.1
+
 # to use a specific profile e.g. 'dev'
-session = boto3.session.Session(profile_name='dev')
-cd = session.client('ce', 'us-east-1')
+for profile in profiles:
+    session = boto3.session.Session(profile_name=profile)
+    cd = session.client('ce', 'us-east-1')
 
-results = []
+    results = []
+    token = None
+    while True:
+        if token:
+            kwargs = {'NextPageToken': token}
+        else:
+            kwargs = {}
+        data = cd.get_cost_and_usage(TimePeriod={'Start': start, 'End':  end}, Granularity=granularity, Metrics=['UnblendedCost'], GroupBy=[{'Type': 'DIMENSION', 'Key': 'LINKED_ACCOUNT'}, {'Type': 'DIMENSION', 'Key': 'SERVICE'}], **kwargs)
+        results += data['ResultsByTime']
+        token = data.get('NextPageToken')
+        if not token:
+            break
+    
+    filtered_results[profile] = []
+    profiles_results[profile] = results
+    for result_by_time in results:
+        for group in result_by_time['Groups']:
+            amount = group['Metrics']['UnblendedCost']['Amount']
+            if float(amount) < MIN_COST:
+                continue  
+            unit = group['Metrics']['UnblendedCost']['Unit']
+            filtered_results[profile].append([result_by_time['TimePeriod']['Start'], group['Keys'][0], group['Keys'][1], amount]) #, unit, result_by_time['Estimated']])
 
-token = None
-while True:
-    if token:
-        kwargs = {'NextPageToken': token}
-    else:
-        kwargs = {}
-    data = cd.get_cost_and_usage(TimePeriod={'Start': start, 'End':  end}, Granularity='DAILY', Metrics=['UnblendedCost'], GroupBy=[{'Type': 'DIMENSION', 'Key': 'LINKED_ACCOUNT'}, {'Type': 'DIMENSION', 'Key': 'SERVICE'}], **kwargs)
-    results += data['ResultsByTime']
-    token = data.get('NextPageToken')
-    if not token:
-        break
-
-print('\t'.join(['TimePeriod', 'LinkedAccount', 'Service', 'Amount', 'Unit', 'Estimated']))
-for result_by_time in results:
-    for group in result_by_time['Groups']:
-        amount = group['Metrics']['UnblendedCost']['Amount']
-        unit = group['Metrics']['UnblendedCost']['Unit']
-        print(result_by_time['TimePeriod']['Start'], '\t', '\t'.join(group['Keys']), '\t', amount, '\t', unit, '\t', result_by_time['Estimated'])
+#print(profiles_results)
+print(json.dumps(filtered_results))
